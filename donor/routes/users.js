b1e32f1bd62bb1cc9113aeb3c6f2bf816ad55c71
ATSivapraneeth <atmakuripraneeth@gmail.com>
2020-12-12 18:49:25 +0530

    first commit

---
+++ donor/routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');  
+const { response } = require('../app');
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+/* Process POST Form */
+var urlencodedParser = bodyParser.urlencoded({ extended: false });  
+
+router.post('/process', urlencodedParser, function (req, res) {  
+   var Blood_Group = req.body.bgroup; 
+   var result;
+   var name=req.body.name;
+  if(Blood_Group=='A' || Blood_Group=='B' || Blood_Group=='O' || Blood_Group=='AB')
+    result = 'Blood group is available';
+  else  
+    result = 'Blood group not available';
+
+   res.render('response',  {name : name, message: result});
+  
+});
+router.post('/process2', urlencodedParser, function (req, res) {  
+   var Blood_Group = req.body.bgroup; 
+   var name= req.body.rname;
+   var result;
+  if(Blood_Group=='A' || Blood_Group=='B' || Blood_Group=='O' || Blood_Group=='AB')
+    result = 'Blood group is available';
+  else  
+    result = 'Blood group not available';
+
+   res.render('response',  {name : name, message: result});
+  
+});  
+
+module.exports = router;
